74425e7cd027d87de9f078d362d6f28c1bf20e26
Enrique <sotomaque@gmail.com>
2021-02-05 01:48:51 -0800

    test api integration;

--- src/app/pages/Home/component.jsx
+++ src/app/pages/Home/component.jsx
@@ -1,16 +1,55 @@
 /* eslint-disable no-unused-vars */
-import { useQuery } from '@apollo/react-hooks';
-import { NUMBER_OF_USERS_QUERY } from 'app/gql/queries';
 import React from 'react';
 
+import { useQuery } from '@apollo/react-hooks';
+import { NUMBER_OF_USERS_QUERY } from 'app/gql/queries';
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
 const HomePage = () => {
+  const [state, setState] = React.useState({});
   const { loading, error, data } = useQuery(
     NUMBER_OF_USERS_QUERY,
   );
   if (data) {
     console.log(data);
   }
-  return <div>Home Page</div>;
+
+  React.useEffect(async () => {
+    const res = await fetch(
+      'https://api.coindesk.com/v1/bpi/historical/close.json',
+    );
+    const data = await res.json();
+    setState({
+      data: Object.keys(data.bpi).map((date) => {
+        return {
+          date,
+          price: data.bpi[date],
+        };
+      }),
+    });
+  }, []);
+  if (state) {
+    console.log(state);
+  }
+  const padding = 100;
+  const xMax = width - padding;
+  const yMax = height - padding;
+
+  return (
+    <div>
+      <svg width={width} height={height}>
+        <rect
+          x={0}
+          y={0}
+          width={width}
+          height={height}
+          fill="#32deaa"
+        />
+      </svg>
+    </div>
+  );
 };
 
 export default HomePage;
